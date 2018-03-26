12589d3a516e1aaf63b93c87502e61a5c214b3c5
el3um4s <el3um4s@gmail.com>
2018-03-26 19:44:41 +0200

    iniziato a lavorare su input da tastiera

--- src/renderer/components/InfosView.vue
+++ src/renderer/components/InfosView.vue
@@ -26,6 +26,7 @@
           <li class="itemInElenco openLink" @click.prevent="openLink('http://mikemcl.github.io/decimal.js/')">decimal.js</li>
           <li class="itemInElenco openLink" @click.prevent="openLink('https://fonts.google.com/specimen/Roboto')">Google Font Roboto</li>
           <li class="itemInElenco openLink" @click.prevent="openLink('https://fonts.google.com/specimen/Mina')">Google Font Mina</li>
+          <li class="itemInElenco openLink" @click.prevent="openLink('https://github.com/Dafrok/v-hotkey')">v-hotkey</li>
         </div>
       </v-card-text>
     </v-card>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 import Vuetify from 'vuetify'
+import VueHotkey from 'v-hotkey'
+
 import 'vuetify/dist/vuetify.css'
 import 'mdi/css/materialdesignicons.min.css'
 
@@ -8,8 +10,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-// Vue.use(Vuetify)
 Vue.use(Vuetify)
+Vue.use(VueHotkey)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
